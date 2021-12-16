387a47a7d6e7d47df0239c203c8155f0bd622a28
vejron <bjorn.yttergren@gmail.com>
2021-12-16 22:23:43 +0100

    nuxi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'lambda'
-  },
   buildModules: [
     '@vueuse/nuxt',
     'nuxt-windicss',
